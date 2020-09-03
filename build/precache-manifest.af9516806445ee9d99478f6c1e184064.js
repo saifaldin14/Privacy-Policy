28bc0d6dc0e5bd3053b7a26fd12eb875f44f6d4e
Saif Al-Din Ali <saif-canada@hotmail.com>
2020-09-03 08:00:21 -0400

    Add files via upload

---
+++ build/precache-manifest.af9516806445ee9d99478f6c1e184064.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5579e3a5c3cc0f5a8d39f76bee549fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6f817ee1697c46a3a10",
+    "url": "/static/css/main.ddaec5d5.chunk.css"
+  },
+  {
+    "revision": "8a23cc8452373b5eb6dc",
+    "url": "/static/js/2.26fe5186.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26fe5186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f817ee1697c46a3a10",
+    "url": "/static/js/main.e93a7825.chunk.js"
+  },
+  {
+    "revision": "9f52df0eae2d0a0953fb",
+    "url": "/static/js/runtime-main.ba34c5c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
